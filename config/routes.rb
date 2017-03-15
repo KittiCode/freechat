a65728a6e39dabbb2df476e7523db2cd7a0ee934
KittiShane <chain_chainna@hotmail.com>
2017-03-16 08:59:01 +0900

    Added User Registration and Chatroom's index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'chatrooms', to: 'chatrooms#index'
+	root 'welcome#about'
+get '/signup', to: "registrations#new"
+  post '/signup', to: "registrations#create"
+
+  get 'login', to: "sessions#new"
+  get 'users', to: 'users#new'
+
+resources :chatrooms, param: :slug
+
+ 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
